5ca74a9a8bc693394217e471ef7027655945b0ca
Terry Law <tl@ixx.cc>
2015-11-05 18:45:54 +0800

    add: send enquiry function

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,7 +6,7 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($http, $timeout, webDevTec, toastr) {
     var vm = this;
     
     vm.dimension = {
@@ -15,12 +15,62 @@
         height: 0,
         weight: 0
     }
+    vm.priceList = "1";
+    vm.mail = {
+        name: "",
+        email: "",
+        phone: "",
+        member_id: "",
+        content: ""
+    }
+    
+    vm.getFreight = getFreight;
+    vm.sendMail = sendMail;
+    
+    
 
     activate();
 
     function activate() {
       
       
+    }
+    
+    function getFreight(){
+        if(vm.dimension.weight == 0){
+            return "-";
+        }
+        if(vm.priceList == "1"){
+            return "RMB " + (13 + Math.max(Math.ceil(vm.dimension.weight) - 1, 0)*6);
+        }else if(vm.priceList == "2"){
+            return "RMB " + Math.max(Math.ceil(vm.dimension.weight) - 0, 100)*5;
+        }else if(vm.priceList == "3"){
+            return "HKD " + (14 + ((vm.dimension.weight-1)*8));
+        }
+    }
+    
+    function sendMail(){
+        console.log(vm.mail);
+        
+        $http({
+            method: 'POST',
+            url: '//tochangeapp.easy-cheque.com/enquiry',
+            data: vm.mail
+        })
+        .success(function(data, status, headers, config){
+            alert("message sent");
+            
+            vm.mail = {
+                name: "",
+                email: "",
+                phone: "",
+                member_id: "",
+                content: ""
+            }
+        })
+        .error(function(data, status, headers, config){
+            alert("please try again later.");
+        })
     }
 
     
