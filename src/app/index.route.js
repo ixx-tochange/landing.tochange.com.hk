1c0cc3250d1c8229fb52e7f5faa57034ad5a7f98
Terry Law <tl@ixx.cc>
2015-11-05 13:37:38 +0800

    added: inner page html template

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,10 +8,40 @@
   /** @ngInject */
   function routeConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
+      .state('landing', {
+            abstract: true,
+            templateUrl: 'app/layout/default.tmpl.html'
+      })
+        
+      .state('landing.home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController as vm'
+      })
+      
+      .state('landing.tutorial', {
+        url: '/tutorial',
+        templateUrl: 'app/main/tutorial.html'
+      })
+      .state('landing.price-list', {
+        url: '/price-list',
+        templateUrl: 'app/main/price-list.html'
+      })
+      .state('landing.address', {
+        url: '/address',
+        templateUrl: 'app/main/address.html'
+      })
+      .state('landing.faq', {
+        url: '/faq',
+        templateUrl: 'app/main/faq.html'
+      })
+      .state('landing.alipay', {
+        url: '/alipay',
+        templateUrl: 'app/main/alipay.html'
+      })
+      .state('landing.news', {
+        url: '/news',
+        templateUrl: 'app/main/news.html'
       });
 
     $urlRouterProvider.otherwise('/');
